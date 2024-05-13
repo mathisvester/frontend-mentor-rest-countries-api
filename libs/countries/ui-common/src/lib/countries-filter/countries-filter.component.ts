5a072ad44f3bbbb27cc1f6613cd59dee93518e3e
Mathis Vester <26164587+mathisvester@users.noreply.github.com>
2024-05-13 09:16:40 +0200

    feat: adjust countries filter ui

--- libs/countries/ui-common/src/lib/countries-filter/countries-filter.component.ts
+++ libs/countries/ui-common/src/lib/countries-filter/countries-filter.component.ts
@@ -1,8 +1,19 @@
-import { Component, model } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, ElementRef, Inject, model, ViewChild } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { merge, scan, skip, startWith, Subject } from 'rxjs';
-import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import {
+  filter,
+  fromEvent,
+  map,
+  merge,
+  Observable,
+  scan,
+  startWith,
+  Subject,
+} from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+export type DropdownEvent = 'TOGGLE' | 'HIDE';
 
 @Component({
   selector: 'lib-countries-filter',
@@ -12,18 +23,40 @@ import { toObservable, toSignal } from '@angular/core/rxjs-interop';
   styleUrl: './countries-filter.component.scss',
 })
 export class CountriesFilterComponent {
+  @ViewChild('dropdownToggle', { static: true }) dropdownToggle:
+    | ElementRef<HTMLButtonElement>
+    | undefined;
+
   readonly regions = ['Africa', 'Asia', 'Europe', 'Oceania'];
   readonly dropdownButtonClick$$ = new Subject<void>();
   readonly selectedRegion = model('');
 
-  isOpen = toSignal(
-    merge(
-      this.dropdownButtonClick$$,
-      toObservable(this.selectedRegion).pipe(skip(1))
-    ).pipe(
-      scan(acc => !acc, false),
+  private readonly toggleDropdown$: Observable<DropdownEvent> =
+    this.dropdownButtonClick$$.pipe(map(() => 'TOGGLE'));
+  private readonly hideDropdown$: Observable<DropdownEvent> = fromEvent(
+    this.document,
+    'click'
+  ).pipe(
+    filter(e => e.target !== this.dropdownToggle?.nativeElement),
+    map(() => 'HIDE')
+  );
+
+  readonly isOpen = toSignal(
+    merge(this.toggleDropdown$, this.hideDropdown$).pipe(
+      scan((acc, value) => {
+        switch (value) {
+          case 'TOGGLE':
+            return !acc;
+          case 'HIDE':
+            return false;
+          default:
+            return acc;
+        }
+      }, false),
       startWith(false)
     ),
     { initialValue: false }
   );
+
+  constructor(@Inject(DOCUMENT) private readonly document: Document) {}
 }
